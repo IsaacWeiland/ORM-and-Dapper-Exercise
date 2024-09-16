7543a40de0bcfcdb392184abd706d4e5a6768975
Isaac Weiland <iweiland7777@gmail.com>
2024-09-16 10:36:29 -0400

    Fixed all issues will work fine

--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -16,13 +16,13 @@ namespace ORM_Dapper
             IDbConnection conn = new MySqlConnection(connString);
             // Creates an instance of the DDR \/
             var departmentRepo = new DapperDepartmentRepository(conn);
-
+            departmentRepo.InsertDepartment("New Really Cool Department");
             var departments = departmentRepo.GetAllDepartments();
             foreach (var department in departments)
             {
                 Console.WriteLine(department.DepartmentID);
                 Console.WriteLine(department.Name);
-                Console.WriteLine("/n");
+                Console.WriteLine("\n");
             }
         }
     }
--- ORM-Dapper/obj/Debug/net6.0/ORM-Dapper.AssemblyInfo.cs
+++ ORM-Dapper/obj/Debug/net6.0/ORM-Dapper.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ORM-Dapper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ea2b2f60efbc58716051b44713dfb2718df8862")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+378544d2c7a3c4c022bb8e3979ae302ade236da0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ORM-Dapper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ORM-Dapper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
