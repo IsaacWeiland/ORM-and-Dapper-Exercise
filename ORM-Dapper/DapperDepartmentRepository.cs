378544d2c7a3c4c022bb8e3979ae302ade236da0
Isaac Weiland <iweiland7777@gmail.com>
2024-09-16 10:27:39 -0400

    unworking copy

---
+++ ORM-Dapper/DapperDepartmentRepository.cs
@@ -0,0 +1,23 @@
+using System.Data;
+using Dapper;
+
+namespace ORM_Dapper;
+
+public class DapperDepartmentRepository : IDepartmentRepository
+{
+    private readonly IDbConnection _conn;
+
+    public DapperDepartmentRepository(IDbConnection conn)
+    {
+        _conn = conn;
+    }
+
+    public IEnumerable<Department> GetAllDepartments()
+    {
+        return _conn.Query<Department>("SELECT * FROM departments");
+    }
+    public void InsertDepartment(string deptName)
+    {
+        _conn.Execute("INSERT INTO departments (Name) VALUES (@name)", new {name = deptName});
+    }
+}
